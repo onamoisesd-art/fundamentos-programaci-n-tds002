0977367451992ee5ca07b4301945daf17e99c16b
Onasies Díaz <onamoisesd@gmail.com>
2026-08-25 22:31:28 -0400

    Ejercicio 1: Tarjeta de presentación

---
+++ ProyectoS1/Program.cs
@@ -0,0 +1,29 @@
+﻿/*
+Nombre: Onasies
+Fecha: 25/08/2026
+Descripción: Programa que imprime una tarjeta de presentación
+con mis datos personales y una frase que me describe.
+*/
+
+using System;
+
+class Program
+{
+    static void Main()
+    {
+        // Imprime el marco superior
+        Console.WriteLine("****************************");
+        
+        // Imprime los datos personales
+        Console.WriteLine("* Nombre: Onasies          *");
+        Console.WriteLine("* Matrícula: 2026-1234     *");
+        Console.WriteLine("* Carrera: Informática     *");
+        
+        // Frase que te describe
+        Console.WriteLine("* \"Aprendiendo paso a paso\" *");
+        
+        // Imprime el marco inferior
+        Console.WriteLine("****************************");
+    }
+}
+
